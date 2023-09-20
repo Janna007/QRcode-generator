d307ee8c7ec6beef8782766de4693919abbe6482
janna <jannakondeth5@gmail.com>
2023-09-20 11:59:17 +0530

    add file to save url

--- 2.4 QR Code Project/index.js
+++ 2.4 QR Code Project/index.js
@@ -15,8 +15,14 @@
   .then((answers) => {
     // Use user feedback for... whatever!!
    const url=answers.URL;
+   //2. Use the qr-image npm package to turn the user entered URL into a QR code image.
    var qr_svg = qr.image(url);
     qr_svg.pipe(fs.createWriteStream("qr_img.png"));
+    //3. Create a txt file to save the user input using the native fs node module.
+    fs.writeFile('URL.txt', url, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+      }); 
   })
   .catch((error) => {
     if (error.isTtyError) {
@@ -26,7 +32,7 @@
     }
   });
 
-  
+ 
 
 
 
@@ -34,7 +40,3 @@
 
 
 
-/*
-2. Use the qr-image npm package to turn the user entered URL into a QR code image.
-3. Create a txt file to save the user input using the native fs node module.
-*/
